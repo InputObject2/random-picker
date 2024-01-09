a2f43901282451bb91d67db5a5eee00539c1fa12
InputObject2 <30133702+InputObject2@users.noreply.github.com>
2024-01-09 00:06:53 -0500

    Added api for those special use cases

--- main.go
+++ main.go
@@ -1,15 +1,55 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
+
+type ItemList struct {
+	Items []string `json:"items"`
+}
+
+func randomItemHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var list ItemList
+	err := json.NewDecoder(r.Body).Decode(&list)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if len(list.Items) == 0 {
+		http.Error(w, "List is empty", http.StatusBadRequest)
+		return
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	randomIndex := rand.Intn(len(list.Items))
+	selectedItem := list.Items[randomIndex]
+
+	response, err := json.Marshal(map[string]string{"selectedItem": selectedItem})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "index.html")
 	})
+	http.HandleFunc("/random", randomItemHandler)
 
-	log.Println("Starting web server on http://localhost:8080")
+	log.Println("Starting web server on http://localhost:8080/ and api is available for a json list format on http://localhost:8080/random")
 	http.ListenAndServe(":8080", nil)
 }
--- readme.md
+++ readme.md
@@ -13,8 +13,26 @@ Note that there is no session management implemented, so refreshing the page wil
 
 ## Running with Docker
 
-You can run the container and it will :
+You can run the container this way:
 
 ```shell
 docker run -p 8080:8080 inputobject2/random-picker
+```
+
+## API Usage
+
+There are two ways to use this randomizer. The first is to go on the web page directly with a web browser, type in your items and hit random. Refresh the page to clear it out.
+
+The second is with the API by sending a POST with a json body to `/random`, the API will return a random object from the list you sent.
+
+```shell
+curl -X POST http://localhost:8080/random -d '{"items":["apple", "banana", "cherry"]}' -H "Content-Type: application/json"
+```
+
+Server responds with:
+
+```json
+{
+    "selectedItem":"banana"
+}
 ```
